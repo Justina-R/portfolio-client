80f361abeac42f670b1afd1a396521463071a3b1
Justina-R <justina.rey2602@gmail.com>
2026-04-16 14:33:04 -0300

    Add Vercel analytics

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import MainLayout from "./components/mainLayout/MainLayout";
 import { useEffect, useState } from "react";
 import Loader from "./components/loader/Loader";
+import { Analytics } from "@vercel/analytics/react";
 
 function App() {
   const [loading, setLoading] = useState(true);
@@ -16,9 +17,12 @@ function App() {
   }
 
   return (
-    <Routes>
-      <Route path="/" element={<MainLayout />} />
-    </Routes>
+    <>
+      <Routes>
+        <Route path="/" element={<MainLayout />} />
+      </Routes>
+      <Analytics />
+    </>
   );
 }
 
